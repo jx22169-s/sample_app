335bac00b57ac298919da7f6d3213deef4554d49
jx22169-s <jx22169@ns.kogakuin.ac.jp>
2024-11-26 05:39:57 +0000

    Finish user signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,23 +2,15 @@ require "test_helper"
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
 
-  test "invalid signup information" do
-    get signup_path
-    assert_no_difference 'User.count' do
-      post users_path, params: { user: { name:  "",
-                                         email: "user@invalid",
-                                         password:              "foo",
-                                         password_confirmation: "bar" } }
+  test "valid signup information" do
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  "Example User",
+                                         email: "user@example.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
     end
     follow_redirect!
     assert_template 'users/show'
-    # ステータスコードが422（Unprocessable Entity）であることを確認
-    assert_response :unprocessable_entity
-    # エラーが発生した際に表示されるテンプレートが `users/new` であることを確認
-    assert_template 'users/new'
-    # エラー表示用のCSSセレクタが正しいことを確認
-    assert_select 'div#error_explanation'
-    # エラーに関連するCSSクラス（例: `field_with_errors`）が存在することを確認
-    assert_select 'div.field_with_errors'
+    assert_not flash.empty?
   end
 end
