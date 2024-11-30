67cd209fada46826ef99254ce270207dfe8d70ca
jx22169-s <jx22169@ns.kogakuin.ac.jp>
2024-11-30 10:34:07 +0000

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,10 +17,11 @@ class UsersController < ApplicationController
       render 'new', status: :unprocessable_entity
     end
   end
+
   private
 
-  def user_params
-    params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
-  end
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -10,15 +10,14 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password:              "foo",
                                          password_confirmation: "bar" } }
     end
-    follow_redirect!
-    assert_template 'users/show'
+    # follow_redirect! は不要なので削除
+    # 'new' テンプレートが再表示されていることを確認
+    assert_template 'users/new'
     # ステータスコードが422（Unprocessable Entity）であることを確認
     assert_response :unprocessable_entity
-    # エラーが発生した際に表示されるテンプレートが `users/new` であることを確認
-    assert_template 'users/new'
     # エラー表示用のCSSセレクタが正しいことを確認
     assert_select 'div#error_explanation'
-    # エラーに関連するCSSクラス（例: `field_with_errors`）が存在することを確認
+    # エラーに関連するCSSクラスが存在することを確認
     assert_select 'div.field_with_errors'
   end
 end
